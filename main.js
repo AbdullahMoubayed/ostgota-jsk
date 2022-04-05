77b020ac2a4e79721704d59b9fa88d8c37cc5d12
Abdullah Moubayed <93073119+AbdullahMoubayed@users.noreply.github.com>
2022-04-05 14:55:16 +0200

    home page desktop

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import './css/style.css';
 
 let listIcon = document.querySelector('.list-icon');
+let dropdownIcon = document.querySelectorAll('.dropdown-icon');
 
 //toggle nav menu
 listIcon.addEventListener('click', () => {
   listIcon.classList.toggle('active');
   document.querySelector('.nav-body').classList.toggle('open');
+});
+
+//toggle sropdown menu
+dropdownIcon.forEach(icon => {
+  icon.addEventListener('click', () => {
+    icon.classList.toggle('active');
+    icon.nextElementSibling.classList.toggle('open');
+  });
 });
