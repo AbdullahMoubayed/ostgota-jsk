2b5765a7eb1a45c61d4880e5eb191b1967449316
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2022-04-05 12:08:14 +0200

    designing header

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './css/style.css';
+
+let listIcon = document.querySelector('.list-icon');
+
+//toggle nav menu
+listIcon.addEventListener('click', () => {
+  listIcon.classList.toggle('active');
+  document.querySelector('.nav-body').classList.toggle('open');
+});
